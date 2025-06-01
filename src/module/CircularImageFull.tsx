6aa3d032bd98f8743827b32b0d02c7c421a60c31
Petr Elshin <Petakioto2.0@gmail.com>
2025-06-02 00:45:07 +0800

    Update photo image

--- src/module/CircularImageFull.tsx
+++ src/module/CircularImageFull.tsx
@@ -12,11 +12,13 @@ const CircularImageFull: React.FC<CircularImageProps> = ({
                                                          alt
                                                      }) => {
     return (
-        <div className="relative w-[80%] h-[80%] border-gray-700 sm:w-80 md:w-84 lg:w-100 sm:h-80 md:h-84 lg:h-100 rounded-full overflow-hidden">
+        <div className="relative w-[80%] h-[80%] sm:w-80 md:w-84 lg:w-100 sm:h-80 md:h-84 lg:h-100 rounded-full overflow-hidden">
             <Image
                 src={myPhoto}
                 alt={alt}
                 fill
+                objectFit="cover" // Ensures the image maintains aspect ratio
+                objectPosition="center" // Centers the image within the container
                 placeholder="blur"
             />
         </div>
